273de7cf97109419b722cbfa0a88aeb6ebfa040b
meehl <12648828+meehl@users.noreply.github.com>
2025-12-26 08:46:33 +0100

    Update compatibility info for weekly beta releases

--- Compatibility_pob2.lua
+++ Compatibility_pob2.lua
@@ -1,8 +1,8 @@
 -- PoB2 version -> minimum required Rusty PoB version
 local compat = { }
 compat["0.12.2"] = "0.1.0"
-compat["0.12.2-79fc9cbde"] = "0.2.9"
 compat["0.13.0"] = "0.2.10"
 compat["0.14.0"] = "0.2.10"
+compat["0.14.0-2e84d4376"] = "0.2.10"
 
 return compat
