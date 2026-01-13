dde0dbef9af15a56cb5c409934af6de3bb0c8efa
meehl <12648828+meehl@users.noreply.github.com>
2026-01-13 22:34:18 +0100

    Add compat info for PoB2 0.15.0

--- Compatibility_pob2.lua
+++ Compatibility_pob2.lua
@@ -3,6 +3,7 @@ local compat = { }
 compat["0.12.2"] = "0.1.0"
 compat["0.13.0"] = "0.2.10"
 compat["0.14.0"] = "0.2.10"
+compat["0.15.0"] = "0.2.10"
 compat["0.14.0-2e84d4376"] = "0.2.10"
 
 return compat
